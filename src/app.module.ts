d13120c721750efedd7d47213a0a6aea54efbc25
Kaliszby <sumyung17qwq@gmail.com>
2022-05-24 03:00:04 +0700

    fix: env mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), UrlModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.oumzn.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
+    ),
+    UrlModule,
+  ],
 })
 export class AppModule {}
